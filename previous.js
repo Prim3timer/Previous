660a3a2ad6216965d03ea7bc551fc20b2c0ed753
Prim3timer <67154046+Prim3timer@users.noreply.github.com>
2023-07-30 09:50:11 -0400

    Update previous.js
    Copied and pasted  7 lines. That way I'll have something to campare.

--- previous.js
+++ previous.js
@@ -1,1 +1,8 @@
+import {questions} from '/prequiz.js'
+let mainBoard = document.getElementById("main");
+let theScore = document.getElementById("score");
+let score = 0;
+let i = 0;
+let review = 0;
+let anArray = [];
 
